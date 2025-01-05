b2a10416aa6a53015060956febce544d581f3ae8
AmirhosseinKhazaee <bahkh.12345678@example.com>
2025-01-05 15:11:15 +0330

    GradientDescent version 3 Added (two variables gradient)

---
+++ Phase_1_Math_Foundations/gradient_descent_v3.py
@@ -0,0 +1,24 @@
+def minus(x,y):
+    xy = []
+    for i in range(len(x)):
+        xy.append(x[i]-y[i])
+    return xy
+def multiply(alpha,g):
+    xy = []
+    for i in g:
+        xy.append(alpha*i)
+    return(xy)
+gradient = lambda x,y : [(2*x) -4,(2*y) + 6]
+alpha = 0.01
+i = 0
+x0 = 0
+y0 = 0
+while i < 10:
+    x = [x0,y0]
+    y = multiply(alpha,gradient(x0,y0))
+    new_answer = minus(x,y)
+    print(new_answer)
+    x0 = new_answer[0]
+    y0 = new_answer[1]
+    if abs(x[0]-new_answer[0]) < 0.0005 :
+        break
