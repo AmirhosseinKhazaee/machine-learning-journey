4119b833ebdc96944a1b605b762987bba757214f
AmirhosseinKhazaee <bahkh.12345678@example.com>
2025-01-06 00:55:02 +0330

    make it usable library

--- Phase_1_Math_Foundations/gradient_descent_v3.py
+++ Phase_1_Math_Foundations/gradient_descent_v3.py
@@ -1,3 +1,18 @@
+def main():
+# example data
+    gradient = lambda x,y : [(2*x) -4,(2*y) + 6]
+    alpha = 0.01
+    x0 = 0
+    y0 = 0
+    while True:
+        x = [x0,y0]
+        y = multiply(alpha,gradient(x0,y0))
+        new_answer = minus(x,y)
+        print(new_answer)
+        x0 = new_answer[0]
+        y0 = new_answer[1]
+        if abs(x[0]-new_answer[0]) < 0.0005 :
+            break
 def minus(x,y):
     xy = []
     for i in range(len(x)):
@@ -8,17 +23,5 @@ def multiply(alpha,g):
     for i in g:
         xy.append(alpha*i)
     return(xy)
-gradient = lambda x,y : [(2*x) -4,(2*y) + 6]
-alpha = 0.01
-i = 0
-x0 = 0
-y0 = 0
-while i < 10:
-    x = [x0,y0]
-    y = multiply(alpha,gradient(x0,y0))
-    new_answer = minus(x,y)
-    print(new_answer)
-    x0 = new_answer[0]
-    y0 = new_answer[1]
-    if abs(x[0]-new_answer[0]) < 0.0005 :
-        break
+if __name__ == "__main__":
+    main()
